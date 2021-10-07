5561f17750b92c3b8dc3b10477a60fd158d33a99
BenoitDossoine <benoit.dossoine@student.ehb.be>
2021-10-07 17:05:25 +0200

    Score is updated when game is won

--- session1/rock_paper_scissors/src/main/kotlin/Main.kt
+++ session1/rock_paper_scissors/src/main/kotlin/Main.kt
@@ -1,68 +1,113 @@
 fun main(){
     val choices = arrayOf("Rock", "Paper", "Scissors", "Lizard", "Spock")
-    val randomChoice = choices.random()
-    val userChoice = readLine()
-    println("The computer has chosen $randomChoice")
-    println("You have chosen $userChoice")
+    var userScore = 0
 
-    checkResult(randomChoice,userChoice)
+    while(true){
+        var randomChoice = choices.random()
 
+        println("Make your choice:")
+        var userChoice = readLine()
+
+
+        println("The computer has chosen $randomChoice")
+        println("You have chosen $userChoice")
+
+        var result = checkResult(randomChoice,userChoice)
+        if(result == true){
+            userScore ++
+        }
+        println("Your score: $userScore")
+    }
 }
 
-fun checkResult(computer:String,user:String?){
+fun checkResult(computer:String,user:String?):Boolean?{
     if(user != "Rock" && user !="Paper" && user !="Scissors" && user !="Lizard" && user!="Spock"){
         println("Make sure you type Rock, Paper or Scissors!")
+        return false
     } else if (computer == user){
         println("It's a tie!")
+        return false
     } else if (computer == "Rock") {
         if (user == "Paper") {
             println("You won!")
+            return true
         } else if (user == "Scissors") {
             println("You lost!")
+            return false
         } else if (user == "Spock") {
             println("You won!")
+            return true
         } else if (user == "Lizard"){
             println("You lost!")
+            return false
+        }else {
+            return false
         }
-    }else if (computer == "Paper") {
+    } else if (computer == "Paper") {
         if (user == "Scissors") {
             println("You won!")
+            return true
         } else if (user == "Rock") {
             println("You lost!")
+            return false
         } else if (user == "Spock") {
             println("You lost!")
+            return false
         } else if (user == "Lizard"){
             println("You won!")
+            return true
+        }else {
+            return false
         }
-    }else if (computer == "Scissors"){
+    } else if (computer == "Scissors"){
         if(user == "Rock"){
             println("You won!")
+            return true
         } else if (user == "Paper"){
             println("You lost!")
+            return false
         }else if (user == "Spock") {
             println("You won!")
+            return true
         } else if (user == "Lizard"){
             println("You lost!")
+            return false
+        }else {
+            return false
         }
     } else if (computer == "Spock"){
         if(user == "Rock"){
             println("You lost!")
+            return false
         } else if (user == "Paper"){
             println("You won!")
+            return true
         }else if (user == "Scissors") {
             println("You lost!")
+            return false
         } else if (user == "Lizard"){
             println("You won!")
+            return true
+        }else {
+            return false
         }
     } else if (computer == "Lizard"){
         if(user == "Rock"){
             println("You won!")
+            return true
         } else if (user == "Paper"){
             println("You lost!")
+            return false
         }else if (user == "Spock") {
             println("You lost!")
+            return false
         } else if (user == "Scissors"){
             println("You won!")
+            return true
+        } else {
+            return false
         }
+    } else {
+        return false
     }
 }
