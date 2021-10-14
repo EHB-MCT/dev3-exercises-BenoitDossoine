3c2894ee9bef862c0e3ef250f5f35f4e95a80099
BenoitDossoine <benoit.dossoine@student.ehb.be>
2021-10-14 15:42:25 +0200

    Challenge 2 completed
    Finished challenge 2.
    Added a rollDice function that gives a random number between 1 and 6.

--- session2/adventureGame/src/main/kotlin/Main.kt
+++ session2/adventureGame/src/main/kotlin/Main.kt
@@ -13,10 +13,31 @@ fun challengeOne(){
     val answer = readLine()
 
     if (answers.contains(answer)){
+        challengeTwo()
+    } else {
+        gameOver()
+    }
+}
+
+fun challengeTwo(){
+    println("In order to gain knowledge about Moriarty's whereabouts, we should go to The Lion's Head, a club with a...")
+    println("... peculiar reputation. Maybe we can challenge someone to a chance game to get some intel?")
+    println("That fellow looks like he wants to talk. Let's play his game! Just choose low or high and let him roll the dice.")
+    println("Low or High?")
+    
+    val diceRoll = rollDice()
+    println(diceRoll)
+    val answer = readLine()
+
+    if((diceRoll<4 && answer=="Low") || (diceRoll>3 && answer == "High")){
         win()
     } else {
         gameOver()
     }
+}
+
+fun rollDice():Int{
+    return (1..6).random()
 }
 
 fun win(){
