fc489901c19d1c2c01cf1a19dad4870cd07b578a
BenoitDossoine <benoit.dossoine@student.ehb.be>
2021-10-21 14:28:10 +0200

    Created Subject class

---
+++ session3/client_brief/src/main/kotlin/Subject.kt
@@ -0,0 +1,12 @@
+class Subject (
+    val title: String,
+    val credits: Int,
+    val year: Int,
+    val track: String,
+    val semester: Int,
+    val description: String,
+    val elective: Boolean,
+    val software: Array<String>,
+        ){
+
+}
