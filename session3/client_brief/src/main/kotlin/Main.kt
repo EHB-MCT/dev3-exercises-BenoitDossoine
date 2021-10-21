5295ece615bd2b30559f0913d40f18edba3145da
BenoitDossoine <benoit.dossoine@student.ehb.be>
2021-10-21 14:46:05 +0200

    Completed exercise 1

---
+++ session3/client_brief/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val design1 = Subject("Design 1",
+    5,
+    1,
+    "Design",
+    1,
+     "Je maakt in dit opleidingsonderdeel kennis met de basisprincipes van grafische vormgeving: typografie, kleur, beeld, compositie en de werkprocessen van een grafisch vormgever. We leren deze principes toepassen en maken daarvoor gebruik van de software uit de professionele wereld (Adobe Illustrator en Photoshop).",
+     false,
+        ["Adobe XD"]
+    )
+}
